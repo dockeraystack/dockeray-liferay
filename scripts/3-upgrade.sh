fbc0883f05e32c0ddb7968a2acb838371622e408
Thiago Moreira <tmoreira2020@gmail.com>
2022-03-11 16:56:17 -0300

    Refact upgrade script to be more informative

--- scripts/3-upgrade.sh
+++ scripts/3-upgrade.sh
@@ -16,17 +16,17 @@
 #
 
 
-echo
-echo 'Starting upgrade process'
-echo
-
 sleep 30
 
 set -o errexit
 
-DB_UPGRADE_CLIENT_FOLDER="$LIFERAY_HOME/tools/portal-tools-db-upgrade-client"
-
 if [ "$LIFERAY_RUN_UPGRADE" = true ]; then
+    echo
+    echo 'Starting upgrade process'
+    echo
+
+    DB_UPGRADE_CLIENT_FOLDER="$LIFERAY_HOME/tools/portal-tools-db-upgrade-client"
+
     LOCK_FILE="$LIFERAY_HOME/data/upgrade-$ENVIRONMENT.lock"
     if [ ! -f "$LOCK_FILE" ]; then
         echo $HOSTNAME > "$LOCK_FILE"
@@ -41,4 +41,8 @@ if [ "$LIFERAY_RUN_UPGRADE" = true ]; then
             sleep 60
         done
     fi
+else
+    echo
+    echo 'Aborting upgrade process because LIFERAY_RUN_UPGRADE envvar is set to "false"'
+    echo
 fi
